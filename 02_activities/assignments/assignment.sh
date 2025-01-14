d6f690a79eb0f880d6b92d18b70d6b11680aea8a
eman-nishat <eman.nishat@sickkids.ca>
2025-01-14 12:57:48 -0500

    update typo where coworker code was duplicated

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -36,7 +36,7 @@ mv ./data/raw/user_*.log ./data/processed/user_logs
 mv ./data/raw/event_*.log ./data/processed/event_logs
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm ./data/raw/ipaddr_*
-rm ./data/processed/user_logs/user_ipaddr*rf -rf ./data
+rm ./data/processed/user_logs/user_ipaddr*
 # rf -rf ./data
 # change from coworker-change branch is a typo. should be rm -rf .data
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
